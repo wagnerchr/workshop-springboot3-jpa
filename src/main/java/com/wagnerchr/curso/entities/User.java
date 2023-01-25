976b623bf43eba25e9e34a89ba897ca611b35e59
WagnerChr <wagnerckv@gmail.com>
2023-01-25 09:06:39 -0300

    Order Entity

--- src/main/java/com/wagnerchr/curso/entities/User.java
+++ src/main/java/com/wagnerchr/curso/entities/User.java
@@ -1,12 +1,15 @@
 package com.wagnerchr.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -23,6 +26,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -75,8 +81,12 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
-	// Comparar dois objetos
+	// Compare two objects
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
