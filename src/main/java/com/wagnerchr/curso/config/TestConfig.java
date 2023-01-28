007a2fd4bc1ada24af5666cb05f1468c95b25406
WagnerChr <wagnerckv@gmail.com>
2023-01-28 10:38:36 -0300

    ManyToMany Order Product 2

--- src/main/java/com/wagnerchr/curso/config/TestConfig.java
+++ src/main/java/com/wagnerchr/curso/config/TestConfig.java
@@ -11,10 +11,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.wagnerchr.curso.entities.Category;
 import com.wagnerchr.curso.entities.Order;
+import com.wagnerchr.curso.entities.OrderItem;
 import com.wagnerchr.curso.entities.Product;
 import com.wagnerchr.curso.entities.User;
 import com.wagnerchr.curso.entities.enums.OrderStatus;
 import com.wagnerchr.curso.repositories.CategoryRepository;
+import com.wagnerchr.curso.repositories.OrderItemRepository;
 import com.wagnerchr.curso.repositories.OrderRepository;
 import com.wagnerchr.curso.repositories.ProductRepository;
 import com.wagnerchr.curso.repositories.UserRepository;
@@ -34,6 +36,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -55,10 +60,18 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1); 
 		
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
--- src/main/java/com/wagnerchr/curso/entities/Order.java
+++ src/main/java/com/wagnerchr/curso/entities/Order.java
@@ -2,9 +2,12 @@ package com.wagnerchr.curso.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.wagnerchr.curso.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
@@ -13,6 +16,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,6 +36,10 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	
 	public Order() {
 		
@@ -60,6 +68,7 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
+	
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
@@ -77,6 +86,9 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 	
+	public Set<OrderItem> getItems() {
+		return items;
+	}
 
 	@Override
 	public int hashCode() {
