4a31e49165a56d00f6eff07a1158668b8a81f055
WagnerChr <wagnerckv@gmail.com>
2023-01-29 12:57:53 -0300

    Exception handling findById

---
+++ src/main/java/com/wagnerchr/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.wagnerchr.curso.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.wagnerchr.curso.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+// Intercepts exceptions that occur
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	// That method intercepts any exception of that
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
